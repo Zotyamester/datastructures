e97adb385ac0cab42daf6e37ae2386b81ac9ecdf
Zotyamester <szatmary.zoltan1222@gmail.com>
2019-06-25 10:38:20 +0200

    Stack and queue can be either a list or a vector

--- queue.h
+++ queue.h
@@ -1,37 +1,96 @@
+/**
+ * \file queue.h
+ *
+ * This file contains adapter functions for queues.
+ * The queue's implementation can be either a list or a vector depending on the macro.
+ * (By default its implementations is a vector.)
+ * @author Zoltan Szatmary
+ *
+ **/
+
 #ifndef QUEUE_H
 #define QUEUE_H
 
+/** If the 'LIST_QUEUE' macro is defined, then it's going to be a list. **/
+#ifdef LIST_QUEUE
 #include "list.h"
-
 typedef List Queue;
+#else
+#include "vector.h"
+typedef Vector Queue;
+#endif
 
 inline Queue *queue_create()
 {
+#ifdef LIST_QUEUE
     return list_create();
+#else
+    return vector_create();
+#endif
 }
 inline void queue_push(Queue *queue, int data)
 {
+#ifdef LIST_QUEUE
     list_push_front(queue, data);
+#else
+    vector_push_front(queue, data);
+#endif
 }
 inline void queue_pop(Queue *queue)
 {
+#ifdef LIST_QUEUE
     list_pop_back(queue);
+#else
+    vector_pop_back(queue);
+#endif
 }
 inline int queue_top(Queue *queue)
 {
-    return list_get_back(queue);
+#ifdef LIST_QUEUE
+    return list_back(queue);
+#else
+    return vector_back(queue);
+#endif
+}
+inline size_t queue_size(Queue *queue)
+{
+#ifdef LIST_QUEUE
+    return list_size(queue);
+#else
+    return vector_size(queue);
+#endif
+}
+inline bool queue_empty(Queue *queue)
+{
+#ifdef LIST_QUEUE
+    return list_empty(queue);
+#else
+    return vector_empty(queue);
+#endif
 }
 inline void queue_print(Queue *queue, FILE *fp)
 {
+#ifdef LIST_QUEUE
     list_print(queue, fp);
+#else
+    vector_print(queue, fp);
+#endif
 }
 inline void queue_clear(Queue *queue)
 {
+#ifdef LIST_QUEUE
     list_clear(queue);
+#else
+    vector_clear(queue);
+#endif
 }
 inline void queue_delete(Queue *queue)
 {
+#ifdef LIST_QUEUE
     list_delete(queue);
+#else
+    vector_delete(queue);
+#endif
 }
 
 #endif /* QUEUE_H */
--- stack.h
+++ stack.h
@@ -1,37 +1,96 @@
+/**
+ * \file stack.h
+ *
+ * This file contains adapter functions for stacks.
+ * The stack can be either a list or a vector depending on the macro.
+ * (By default its implementations is a vector.)
+ * @author Zoltan Szatmary
+ *
+ **/
+
 #ifndef STACK_H
 #define STACK_H
 
+/** If the 'LIST_STACK' macro is defined, then it's going to be a list. **/
+#ifdef LIST_STACK
 #include "list.h"
-
 typedef List Stack;
+#else
+#include "vector.h"
+typedef Vector Stack;
+#endif
 
 inline Stack *stack_create()
 {
+#ifdef LIST_STACK
     return list_create();
+#else
+    return vector_create();
+#endif
 }
 inline void stack_push(Stack *stack, int data)
 {
+#ifdef LIST_STACK
     list_push_back(stack, data);
+#else
+    vector_push_back(stack, data);
+#endif
 }
 inline void stack_pop(Stack *stack)
 {
+#ifdef LIST_STACK
     list_pop_back(stack);
+#else
+    vector_pop_back(stack);
+#endif
 }
 inline int stack_top(Stack *stack)
 {
-    return list_get_back(stack);
+#ifdef LIST_STACK
+    return list_back(stack);
+#else
+    return vector_back(stack);
+#endif
+}
+inline size_t stack_size(Stack *stack)
+{
+#ifdef LIST_STACK
+    return list_size(stack);
+#else
+    return vector_size(stack);
+#endif
+}
+inline bool stack_empty(Stack *stack)
+{
+#ifdef LIST_STACK
+    return list_empty(stack);
+#else
+    return vector_empty(stack);
+#endif
 }
 inline void stack_print(Stack *stack, FILE *fp)
 {
+#ifdef LIST_STACK
     list_print(stack, fp);
+#else
+    vector_print(stack, fp);
+#endif
 }
 inline void stack_clear(Stack *stack)
 {
+#ifdef LIST_STACK
     list_clear(stack);
+#else
+    vector_clear(stack);
+#endif
 }
 inline void stack_delete(Stack *stack)
 {
+#ifdef LIST_STACK
     list_delete(stack);
+#else
+    vector_delete(stack);
+#endif
 }
 
 #endif /* STACK_H */
