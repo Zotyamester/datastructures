475b05cc53c1df69b34e3d68b8255c8d957e2882
Zotyamester <szatmary.zoltan1222@gmail.com>
2019-09-27 16:38:17 +0200

    Work on Python module

--- Py/datastructures/tree.py
+++ Py/datastructures/tree.py
@@ -1,43 +1,51 @@
 class TreeNode:
-    def __init__(self, val, parent=None, left=None, right=None):
-        self.val = val
+    def __init__(self, data, parent=None, left=None, right=None):
+        self.data = data
         self.parent = parent
         self.left = left
         self.right = right
     def __str__(self):
-        return str(self.val)
+        return str(self.data)
 
 class Tree:
     def __init__(self):
         self.root = None
-    def node_insert(root, parent, val):
+    def node_insert(self, root, parent, data):
         if root == None:
-            return TreeNode(val, parent)
+            return TreeNode(data, parent)
         else:
-            if val < root.val:
-                root.left = Tree.node_insert(root.left, root, val)
-            elif val > root.val:
-                root.right = Tree.node_insert(root.right, root, val)
+            if data < root.data:
+                root.left = self.node_insert(root.left, root, data)
+            elif data > root.data:
+                root.right = self.node_insert(root.right, root, data)
             return root
-    def insert(self, val):
-        self.root = Tree.node_insert(self.root, None, val)
-    def node_search(root, val)
-    def search(self, val):
-        return Tree.node_search(self.root, val)
-    def node_print(root):
+    def insert(self, data):
+        self.root = self.node_insert(self.root, None, data)
+    def node_search(self, root, data):
+        if root == None:
+            return False
+        if root.data == data:
+            return True
+        elif root.data > data:
+            return self.node_search(root.left)
+        else:
+            return self.node_search(root.right)
+    def search(self, data):
+        return self.node_search(self.root, data)
+    def node_print(self, root):
         if root == None:
             return ''
         s = ''
-        tmp = Tree.node_print(root.left)
+        tmp = self.node_print(root.left)
         if tmp != '':
             s += tmp + ' '
-        s += str(root.val)
-        tmp = Tree.node_print(root.right)
+        s += str(root.data)
+        tmp = self.node_print(root.right)
         if tmp != '':
             s += ' ' + tmp
         return s
     def __str__(self):
-        return Tree.node_print(self.root)
+        return self.node_print(self.root)
     def clear(self):
         self.root = None
 
